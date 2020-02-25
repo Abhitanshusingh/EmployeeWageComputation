7986656e39ebcf70936a78fa56d22da0c74a6e32
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-25 12:10:37 +0530

    MODIFIED: Checking employee is present or absent

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 printf "Welcome to Employee Wage Computation\n"
+#CONSTANT
+ISPRESENT=1
+random=$((RANDOM%2+1))
+if(( $ISPRESENT == $random))			#If random is 1 then present 0 then not present
+then
+	printf "Employee is present \n"
+else
+	printf "Employee Not present\n"
+fi
