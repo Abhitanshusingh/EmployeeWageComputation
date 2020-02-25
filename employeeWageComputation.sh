ab87b834dfcfa4b4eb2a06c269caf372a8a8c00e
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-25 17:43:48 +0530

    Calculating 100hrs and 20 days

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,34 +1,36 @@
 #!/bin/bash -x
 printf "Welcome to Employee Wage Computation\n"
 #CONSTANT
-PERHOUR=20
-FULLDAYHOUR=8
-PARTTIMEHOUR=4
-NUMBEROFWORKINGDAYS=20
-countFullTime=0
-countPartTime=0
-countNotPresent=0
-for(( day=1; day <= $NUMBEROFWORKINGDAYS ; day++ ))
+PER_HOUR=20
+FULL_DAY_HOUR=8
+PART_TIME_HOUR=4
+NUMBER_OF_HOUR=100
+NUMBER_OF_DAYS=20
+#variable
+numberOfDays=20
+totalWorkingDays=0
+totalEmpHrs=0
+#Calculate wages till a condition
+while(( $NUMBER_OF_HOUR > $totalEmpHrs  && $NUMBER_OF_DAYS > $totalWorkingDays  ))
 do
+	totalWorkingDays=$((totalWorkingDays+1))
 	random=$((RANDOM%3+1))
 	#Using case statement
 	case $random in
 		1)
-			calculateDailyEmployeewage=$(( PERHOUR * FULLDAYHOUR ))				#Calcute full time
-			countFullTime=$((countFullTime+1))
+			empHrs=8						 #Calcute full time
 			;;
 
 		2)
-			calculatePartTime=$((PERHOUR * PARTTIMEHOUR ))						 #Calculate part time
-			countPartTime=$((countPartTime+1))
+			empHrs=4						 #Calculate part time
 			;;
 
 		3)
-			countNotPresent=$((countNotPresent+1))
+			empHrs=0
 			;;
 	esac
-done
-echo "Full time $countFullTime days employee wage :$(( calculateDailyEmployeewage*$countFullTime ))"
-echo "Part time $countPartTime days employee wage :$(( calculatePartTime*$countPartTime ))"
-echo "Not present $countNotPresent days"
 
+totalEmpHrs=$(($totalEmpHrs+$empHrs))	# Total employee hour
+done
+totalSalary=$(($totalEmpHrs*$numberOfDays))
+echo "Total Salary is :$totalSalary"
