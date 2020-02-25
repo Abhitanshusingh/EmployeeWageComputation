98e44b68613d661d4bf0071eed59a302015a8325
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-25 16:27:29 +0530

    MODIFIED: Added wages of a month

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,22 +4,31 @@ printf "Welcome to Employee Wage Computation\n"
 PERHOUR=20
 FULLDAYHOUR=8
 PARTTIMEHOUR=4
-random=$((RANDOM%3+1))
-#Using case statement
-case $random in
-	1)
-		printf "Employee is present \n"
-		calculateDailyEmployeewage=$(($PERHOUR*$FULLDAYHOUR))				#Calcute full time
-		printf "Full time : $calculateDailyEmployeewage \n"
-		;;
+NUMBEROFWORKINGDAYS=20
+countFullTime=0
+countPartTime=0
+countNotPresent=0
+for(( day=1; day <= $NUMBEROFWORKINGDAYS ; day++ ))
+do
+	random=$((RANDOM%3+1))
+	#Using case statement
+	case $random in
+		1)
+			calculateDailyEmployeewage=$(( PERHOUR * FULLDAYHOUR ))				#Calcute full time
+			countFullTime=$((countFullTime+1))
+			;;
 
-	2)
-		printf "Employee is present \n"
-		calculatePartTime=$(($PERHOUR*$PARTTIMEHOUR))						 #Calculate part time
-		printf "Patr time employee wage : $calculatePartTime \n"
-		;;
+		2)
+			calculatePartTime=$((PERHOUR * PARTTIMEHOUR ))						 #Calculate part time
+			countPartTime=$((countPartTime+1))
+			;;
 
-	3)
-		printf "Employee not present"
-		;;
-esac
+		3)
+			countNotPresent=$((countNotPresent+1))
+			;;
+	esac
+done
+echo "Full time $countFullTime days employee wage :$(( calculateDailyEmployeewage*$countFullTime ))"
+echo "Part time $countPartTime days employee wage :$(( calculatePartTime*$countPartTime ))"
+echo "Not present $countNotPresent days"
+
